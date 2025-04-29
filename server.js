723dceb1a5fd4d7118028dcef755e0b5b21bd4e0
lameck-cs <141039601+lameck-cs@users.noreply.github.com>
2025-04-29 22:41:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -128,5 +128,5 @@ function shuffleArray(array) {
 }
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
